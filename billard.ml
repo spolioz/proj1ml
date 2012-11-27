34893b001243636345caae6053f310a5b3bebd5c
François Pirot <francois.pirot@ens-lyon.fr>
2012-11-27 13:21:25 +0100

    ajout d'une fonction de recherche dans un quadtree

--- billard.ml
+++ billard.ml
@@ -322,7 +322,7 @@ m.boules.(21).o.x <- 120.;
 m.boules.(21).o.y <- (float_of_int (Graphics.size_y())) /. 2.;;
 draw_billard m;;
 
-m.boules.(21).v.x <- 500.;
+m.boules.(21).v.x <- 5000.;
 m.boules.(21).v.y <- 0.;
 launch m (*done*);;
 
@@ -331,7 +331,7 @@ type surface = float * float * float * float;;
 (* Une surface est un rectangle repéré par ses 
 abscisses et ordonnées minimales et maximales. *)
 
-type quadtree = F of surface*(boule list) | N of quadtree*quadtree*quadtree*quadtree;;
+type quadtree = F of surface*(boule list) | N of surface*quadtree*quadtree*quadtree*quadtree;;
 (* Un quadtree renseigne ses subdivision par structure arborescente, ainsi que la liste
 des boules intersectant chaque subdivision. *)
 
@@ -342,6 +342,10 @@ let rec rayon_max = function
 
 let mem_surface b surf = let (xmin, ymin, xmax, ymax) = surf in
 b.o.x +. b.r > xmin && b.o.x -. b.r < xmax && b.o.y +. b.r > ymin && b.o.y -. b.r < ymax;;
+
+let surf = function
+  |F(surf, l) -> surf
+  |N(surf, t1, t2, t3, t4) -> surf;;
 
 let rec boule_list_intersect surf = function
   | [] -> []
@@ -361,12 +365,23 @@ let rec quadtree_create l =
 	 and surf3 = (x1, xmax, y1, ymax)
 	 and surf1 = (xmin, x1, ymin, y1)
 	 and surf2 = (xmin, x1, y1, ymax) in
-	 N(aux (n-1) surf1 (boule_list_intersect surf1 l), 
+	 N(surf, 
+	   aux (n-1) surf1 (boule_list_intersect surf1 l), 
 	   aux (n-1) surf2 (boule_list_intersect surf2 l), 
 	   aux (n-1) surf3 (boule_list_intersect surf3 l), 
 	   aux (n-1) surf4 (boule_list_intersect surf4 l)) in
   let surf0 = (0., 0., xm, ym) in
 aux n surf0;;
 
-let find_boules_adjacentes b t = match t with
-  |F((xmin, ymin, xmax, ymax), l) -> 
+let rec concat_without_x x l1 l2 = match l1 with
+  | [] -> l2
+  | a::r -> if a = x then concat_without_x x r l2
+    else a::(concat_without_x x r l2);;
+(* Concatène une liste à une autre en supprimant les apparitions de x dans la première. *)
+
+let rec find_boules_adjacentes b = function
+  |F(surf, l) -> if mem_surface b surf then l else []
+  |N(surf, t1, t2, t3, t4) -> if not (mem_surface b surf) then []
+    else concat_without_x b (concat_without_x b (concat_without_x b (concat_without_x b (find_boules_adjacentes b t1) (find_boules_adjacentes b t2)) (find_boules_adjacentes b t3)) (find_boules_adjacentes b t4)) []
+;;
+(* Renvoie la liste des boules qui partagent une surface du quadtree avec b *)
