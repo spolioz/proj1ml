1db52826c05d2212775ed73b9e98944285f65829
François Pirot <francois.pirot@ens-lyon.fr>
2012-12-20 19:46:57 +0100

    on supprime tout

--- angry boules/bouton.ml
+++
@@ -1,7 +0,0 @@
-open Graphics;;
-
-type bouton = {xmin : int; xmax : int; ymin : int; ymax : int; s : string};;
-
-let select_bouton bout =
-  let (x,y) = Graphics.mouse_pos() in
- Graphics.button_down() && x >= bout.xmin && x <= bout.xmax && y >= bout.ymin && y <= bout.ymax;;
